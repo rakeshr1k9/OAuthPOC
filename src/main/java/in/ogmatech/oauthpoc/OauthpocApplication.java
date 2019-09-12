1ce9b92a725f33289076c3a1f6d2519fad037641
unknown <rakeshr1k9@gmail.com>
2019-09-12 12:39:32 +0530

    initial commit

---
+++ src/main/java/in/ogmatech/oauthpoc/OauthpocApplication.java
@@ -0,0 +1,13 @@
+package in.ogmatech.oauthpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ogmatech/oauthpoc/OauthpocApplicationTests.java
@@ -0,0 +1,16 @@
+package in.ogmatech.oauthpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthpocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
